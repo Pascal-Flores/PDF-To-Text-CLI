596590a03795f1dadb36ef0e47a30957b891e737
Pascal Flores <pascal.flores@etu.univ-amu.fr>
2022-12-07 14:19:57 +0100

    removed unused code

--- Parser/pdftotextparser.py
+++ Parser/pdftotextparser.py
@@ -51,25 +51,5 @@ def main():
             
 main()
 
-'''
-
-# generates the txt version of the input file
-subprocess.run(["pdftotext", "-raw", args.file, args.file+'.txt'])
-
-pdftotxt = open(args.file+'.txt')
-# creates the output file
-outfile = None
-if args.output:
-    outfile = open(args.output, 'w')
-else:
-    outfile = open(Path(args.file).stem, 'w')
-
-#write the name of the file with spaced replaced by underscores
-outfile.write(os.path.basename(args.file).replace(' ', '_'))
-
-# deletes the txt version of the input file
-os.remove(args.file+'.txt')
-
-'''
 
 
