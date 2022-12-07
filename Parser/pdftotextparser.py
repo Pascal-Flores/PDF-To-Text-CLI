fa9d2300c899ac0e58b45724460c6e1dee56ea70
Pascal Flores <pascal.flores@etu.univ-amu.fr>
2022-12-07 12:48:58 +0100

    creates temp file for working

--- Parser/pdftotextparser.py
+++ Parser/pdftotextparser.py
@@ -1,13 +1,21 @@
+#!/usr/bin/python
+
 import os
+import subprocess
 import argparse
 from pathlib import Path
 
 parser = argparse.ArgumentParser(description='Convert pdftotext output to a custom txt file')
-parser.add_argument('file', help='a pdf file')
-parser.add_argument('-o', '--output', help='output file')
+parser.add_argument('file', help='a pdf file', type=str)
+parser.add_argument('-o', '--output', help='output file', type=str)
 
 args = parser.parse_args()
 
+# generates the txt version of the input file
+subprocess.run(["pdftotext", "-raw", args.file, args.file+'.txt'])
+
+pdftotxt = open(args.file+'.txt')
+# creates the output file
 outfile = None
 if args.output:
     outfile = open(args.output, 'w')
@@ -16,3 +24,9 @@ else:
 
 #write the name of the file with spaced replaced by underscores
 outfile.write(os.path.basename(args.file).replace(' ', '_'))
+
+# deletes the txt version of the input file
+os.remove(args.file+'.txt')
+
+
+
