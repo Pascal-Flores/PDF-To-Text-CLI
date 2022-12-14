e6d10f9464166ebc80128d913de88e43a8e761f2
Sébastien <sebastien.Re@alumni.univ-avignon.fr>
2022-12-14 09:27:16 +0100

    ajout xml

--- Parser/pdftotextparser.py
+++ Parser/pdftotextparser.py
@@ -8,22 +8,22 @@ from pathlib import Path
 import abstract
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("path", help="path to the directory containing the pdf files")
-    parser.add_argument("-x", "--xml", action="store_true",  help="output file will be in xml format")
-    parser.add_argument("-t", "--txt", action="store_true", help="output file will be in txt format")
+    parser = argparse.ArgumentParser()#argument parser
+    parser.add_argument("path", help="path to the directory containing the pdf files")#argument path
+    parser.add_argument("-x", "--xml", action="store_true",  help="output file will be in xml format")#argument xml
+    parser.add_argument("-t", "--txt", action="store_true", help="output file will be in txt format")#argument txt
 
-    args = parser.parse_args()
+    args = parser.parse_args()#parse les arguments
 
-    path = args.path
-    if args.xml and args.txt:
+    path = args.path#path prend la valeur de l'argument path
+    if args.xml and args.txt:#si les deux arguments sont présents
         print("Error: cannot specify both xml and txt output")
         exit(1)
-    elif args.xml:
+    elif args.xml:#si l'argument xml est présent
         createFiles(path, "xml")
-    elif args.txt:
+    elif args.txt:#si l'argument txt est présent
         createFiles(path, "txt")
-    else:
+    else:#si aucun argument n'est présent
         print("Error: no output format specified")
         exit(1)
     
@@ -72,10 +72,10 @@ def getAuthors(pdftotext_file):
     return "not implemented yet"
 
 def createFiles(inputPath, format):
-    outputPath = inputPath + "_" + format.upper()
-    SanitizeOutputDirectory(outputPath)
-    generateTempFiles(inputPath, outputPath)
-    match format:
+    outputPath = inputPath + "_" + format.upper()#outputPath prend la valeur de l'argument path + _ + xml ou txt
+    SanitizeOutputDirectory(outputPath)#nettoie le dossier de sortie
+    generateTempFiles(inputPath, outputPath)#génère les fichiers temporaires
+    match format:#match le format
         case "xml":
             generateXMLFiles(outputPath)
         case "txt":
@@ -118,20 +118,20 @@ def generateXMLFiles(outputPath):
         if not file.endswith(".pdf.txt"):
             continue
         else:
-            pdftotext_file = open(outputPath+"/"+file, 'r')
-
-        input_file_name = Path(os.path.basename(file)).stem
-
-        output_file = open(outputPath+"/"+Path(input_file_name).stem+".txt", 'w+')
+            pdftotext_file = open(outputPath+"/"+file, 'r')#ouvre le fichier txt
+        input_file_name = Path(os.path.basename(file)).stem#input_file_name prend la valeur du nom du fichier sans l'extension
+        output_file = open(outputPath+"/"+Path(input_file_name).stem+".xml", 'w+')#crée le fichier xml
         print (output_file.name)
 
-        # 
-        output_file.write(input_file_name.replace(' ', '_') + '\n')
-    
-        title = pdftotext_file.readline().strip()+pdftotext_file.readline().strip()
-        output_file.write(title + '\n')
-
-        output_file.write(abstract.readAbstract(pdftotext_file))
+        output_file.write('<article>\n')
+        output_file.write("\t<preamble>" + input_file_name.replace(' ', '_') + '</preamble>\n')#remplace les espaces par des _
+        title = pdftotext_file.readline().strip()+pdftotext_file.readline().strip()#prend la première ligne du fichier txt
+        output_file.write("\t<title>"+title + '</title>\n')#écrit la première ligne dans le fichier txt
+        output_file.write("\t<authors>" + "not implemented yet" + '</authors>\n')#écrit les auteurs dans le fichier txt
+        output_file.write("\t<abstract>"+abstract.readAbstract(pdftotext_file)+"</abstract>\n")#écrit l'abstract dans le fichier txt
+        output_file.write("\t<reference>"+abstract.getReference(pdftotext_file)+"</reference>\n")#écrit les références dans le fichier txt
+        output_file.write('</article>\n')
+        
 
         pdftotext_file.close()
         output_file.close()
